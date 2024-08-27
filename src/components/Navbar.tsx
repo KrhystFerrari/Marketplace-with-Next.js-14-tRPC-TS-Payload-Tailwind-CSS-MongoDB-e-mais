caf3521f019129686d8888aeb779d09d412e8c82
KrhystFerrari <krhyst_ferrari@hotmail.com>
2024-08-27 15:52:09 -0300

    feat: Cart adicionado

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { Icons } from "./Icons";
 import NavItems from "./NavItems";
 import { buttonVariants } from "./ui/button";
+import Cart from "./Cart";
 
 const Navbar = () => {
   const user = null;
@@ -56,7 +57,9 @@ const Navbar = () => {
                       />
                     </div>
                   )}
-                  <div className="ml-4 flow-root lg:ml-6"></div>
+                  <div className="ml-4 flow-root lg:ml-6">
+                    <Cart />
+                  </div>
                 </div>
               </div>
             </div>
