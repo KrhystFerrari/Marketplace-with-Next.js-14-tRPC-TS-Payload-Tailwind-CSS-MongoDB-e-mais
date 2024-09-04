4538daf25f1fb6227f3f95156c706945d2a8db32
KrhystFerrari <krhyst_ferrari@hotmail.com>
2024-09-04 20:57:58 -0300

    feat: pagina de agradecimento e lógica adicionados

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import { nextApp, nextHandler } from "./next-utils";
 import * as trpcExpress from "@trpc/server/adapters/express";
 import { AppRouter } from "./trpc";
 import { inferAsyncReturnType } from "@trpc/server";
+import bodyParser from "body-parser";
+import { IncomingMessage } from "http";
 
 const app = express();
 
@@ -19,7 +21,17 @@ const createContext = ({
 
 export type ExpressContext = inferAsyncReturnType<typeof createContext>;
 
+export type WebhookRequest = IncomingMessage & {rawBody: Buffer};
+
 const start = async () => {
+  const webhookMiddleware = bodyParser.json({
+    verify: (req: WebhookRequest, _, buffer ) => {
+      req.rawBody = buffer;
+    }
+  })
+
+  app.post("/api/webhooks/stripe", webhookMiddleware, stripeWebhookHandler)
+
   const payload = await getPayloadClient({
     initOptions: {
       express: app,
